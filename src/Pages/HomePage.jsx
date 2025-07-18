cf02a2784fae3b9c590fbed0769759bb77f51496
Zunaid Sheikh <lamhaa786@gmail.com>
2025-07-18 12:13:12 +0545

    axios

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,10 +1,35 @@
+import { useEffect, useState } from "react"
 import Footer from "../Components/Footer"
 import Navbar from "../Components/Navbar"
+import axios from "axios"
+import Card from "../Components/Card"
 
 const HomePage=()=>{
+    //cariable to store data from backend(hook)
+    const [users,setUsers]=useState([])
+
+    //logic to fetch data from backend
+    const fetchUsers=async ()=>{
+        const response=await axios.get("http://localhost:7500/users")
+        setUsers(response.data.datas)
+    }
+
+
+    //logic to render data in webpage(hook)
+    useEffect(()=>{
+        fetchUsers()
+    },[])
+
     return(
         <>
             <Navbar/>
+            {
+                users.map((user)=>{
+                    return(
+                        <Card user={user} key={user.id}/>
+                    )
+                })
+            }
             <Footer/>
         </>
     )
