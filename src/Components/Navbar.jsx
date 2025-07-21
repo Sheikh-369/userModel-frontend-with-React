8992adfb9bcfbfd85830d3f2b67eae18688da4e2
Zunaid Sheikh <lamhaa786@gmail.com>
2025-07-21 15:48:37 +0545

    create

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom"
+
 const Navbar=()=>{
     return(
         <>
@@ -21,9 +23,8 @@ const Navbar=()=>{
     <div className="flex-grow" />
     <div className="hidden items-center justify-center gap-6 md:flex">
       <a href="#" className="font-dm text-sm font-medium text-slate-700">Sign in</a>
-      <a href="#" className="rounded-md bg-gradient-to-br from-green-600 to-emerald-400 px-3 py-1.5 font-dm text-sm font-medium text-white shadow-md shadow-green-400/50 transition-transform duration-200 ease-in-out hover:scale-[1.03]">Sign
-        up for free
-      </a>
+      <Link to={`/create-page`} className="rounded-md bg-gradient-to-br from-green-600 to-emerald-400 px-3 py-1.5 font-dm text-sm font-medium text-white shadow-md shadow-green-400/50 transition-transform duration-200 ease-in-out hover:scale-[1.03]">+Create
+      </Link>
     </div>
     <div className="relative flex items-center justify-center md:hidden">
       <button type="button">
--- src/Pages/CreateUser.jsx
+++ src/Pages/CreateUser.jsx
@@ -1,10 +1,97 @@
+import { useState } from "react"
 import Footer from "../Components/Footer"
 import Navbar from "../Components/Navbar"
+import axios from "axios"
+import { useNavigate } from "react-router-dom"
 
 const CreatePage=()=>{
+    const navigate=useNavigate()
+    const [user,setUser]=useState({
+        userName:"",
+        userAge:"",
+        userAddress:"",
+        userImage:""
+    })
+
+    const onUserChange=(e)=>{
+        const {name,value}=e.target
+        setUser({
+            ...user,
+            [name]:value
+        })
+    }
+
+    const handleUserSubmit=async (e)=>{
+        e.preventDefault()
+        const response=await axios.post("http://localhost:7500/users",user)
+        if(response.status===200){
+            alert("User Created Successfully!")
+            navigate("/")
+        }else{
+            alert("Something Went Wrong!")
+        }
+
+    }
     return(
         <>
             <Navbar/>
+            <div className="container mx-auto p4-10">
+  <div className="max-w-md mx-auto bg-white rounded-lg overflow-hidden md:max-w-xl">
+    <div className="md:flex">
+      <div className="w-full px-6 py-8 md:p-8">
+        <h2 className="text-2xl font-bold text-gray-800">Create User</h2>
+        <p className="mt-4 text-gray-600">Please fill out the form below to complete your Information.</p>
+        <form onSubmit={handleUserSubmit} className="mt-6">
+          <div className="mb-6">
+            <label className="block text-gray-800 font-bold mb-2" htmlFor="name">
+              User Name
+            </label>
+            <input 
+            onChange={onUserChange}
+            type="text"
+            name="userName"
+             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="name"  placeholder="Enter Your Name..." />
+          </div>
+          <div className="mb-6">
+            <label className="block text-gray-800 font-bold mb-2" htmlFor="email">
+              User Age
+            </label>
+            <input
+                onChange={onUserChange}
+                name="userAge"
+                type="number"
+                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email"  placeholder="20" />
+          </div>
+          <div className="mb-6">
+            <label className="block text-gray-800 font-bold mb-2" htmlFor="card_number">
+              User Address
+            </label>
+            <input 
+            onChange={onUserChange}
+            name="userAddress"
+            type="text" 
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="card_number"  placeholder="Kathmandu" />
+          </div>
+          <div className="mb-6">
+            <label className="block text-gray-800 font-bold mb-2" htmlFor="card_number">
+              User Image
+            </label>
+            <input 
+            onChange={onUserChange}
+            name="userImage"
+            type="text" 
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="card_number"  placeholder="" />
+          </div>
+          
+          <button className="bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
+  </div>
+            </div>
+
             <Footer/>
         </>
     )
