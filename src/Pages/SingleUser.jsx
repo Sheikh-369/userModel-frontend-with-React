90a2853d720af727b37ab0ad76d3956208863580
Zunaid Sheikh <lamhaa786@gmail.com>
2025-07-22 14:59:01 +0545

    UISinglePAge

--- src/Pages/SingleUser.jsx
+++ src/Pages/SingleUser.jsx
@@ -30,14 +30,27 @@ const SingleUser=()=>{
     return(
         <>
         <Navbar/>
-        <div className="flex justify-center items-center min-h-screen flex-col">
-            <p>{user.userName}</p>
-            <p>{user.userAge}</p>
-            <p>{user.userAddress}</p>
-            <Link to={`/edit-page/${user.id}`}><button>Edit</button></Link>
-            <button onClick={deleteUser}>Delete</button>
-        </div>
+        {/* for image */}
+        {user.userImage &&(
+            <img
+            src={user.userImage}
+            alt="User" className="w-full mt-8 object-contain h-64 bg-gray-100"/>
+        )}
+        {/* for Information */}
         
+          <div className="max-w-md mx-auto bg-white shadow-md rounded p-6 mt-6 text-center">
+            <h2 className="text-xl font-semibold mb-2">{user.userName}</h2>
+            <p><strong>Age:</strong>{user.userAge}</p>
+            <p><strong>Address:</strong>{user.userAddress}</p>
+            {/* <p>{user.userName}</p>
+            <p>{user.userAge}</p>
+            <p>{user.userAddress}</p> */}
+            {/* for buttons */}
+            <div className="flex justify-around mt-4">
+            <Link to={`/edit-page/${user.id}`}><button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">Edit</button></Link>
+            <button onClick={deleteUser} className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600">Delete</button>
+            </div>
+        </div>
         <Footer/>
         </>
     )
